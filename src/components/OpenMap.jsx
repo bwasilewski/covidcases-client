0c5218d0938ad2c035131e4da1c098ced9dfdaf3
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-03-30 17:22:45 -0500

    begin openlayers integration

---
+++ src/components/OpenMap.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import Map from 'ol/Map'
+import View from 'ol/View'
+import TileLayer from 'ol/layer/Tile'
+import XYZ from 'ol/source/XYZ'
+
+const OpenMap = props => {
+
+  useEffect(() => {
+    new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+  })
+
+  return (
+    <div id="map" style={{'width': '100%', 'height': '400px'}}></div>
+  )
+}
+
+export default OpenMap
