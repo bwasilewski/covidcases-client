95d3671e033be4cb9e27e436ff2282fed9eaa418
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-03-31 16:00:51 -0500

    fix centering bug

--- src/components/OpenMap.jsx
+++ src/components/OpenMap.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react'
+import { fromLonLat } from 'ol/proj'
 import Map from 'ol/Map'
 import View from 'ol/View'
 import TileLayer from 'ol/layer/Tile'
 import XYZ from 'ol/source/XYZ'
 
 const OpenMap = props => {
+  const { center, zoom } = props
 
   useEffect(() => {
     new Map({
@@ -17,15 +19,13 @@ const OpenMap = props => {
         })
       ],
       view: new View({
-        center: [0, 0],
-        zoom: 2
+        center: fromLonLat(center),
+        zoom: zoom
       })
-    });
+    })
   })
 
-  return (
-    <div id="map" style={{'width': '100%', 'height': '400px'}}></div>
-  )
+  return <div id="map" style={{'width': '100%', 'height': '800px'}} />
 }
 
 export default OpenMap
