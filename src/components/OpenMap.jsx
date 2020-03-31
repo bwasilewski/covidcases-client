2c021478565ba5dbaa036deb3fb803a73f377e90
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-03-31 18:14:28 -0500

    this logic shouldn't be a component

--- src/components/OpenMap.jsx
+++ src/components/OpenMap.jsx
@@ -1,31 +1,15 @@
 import React, { useEffect } from 'react'
-import { fromLonLat } from 'ol/proj'
-import Map from 'ol/Map'
-import View from 'ol/View'
-import TileLayer from 'ol/layer/Tile'
-import XYZ from 'ol/source/XYZ'
+import { InitMap } from '../events/maps'
 
 const OpenMap = props => {
   const { center, zoom } = props
 
   useEffect(() => {
-    new Map({
-      target: 'map',
-      layers: [
-        new TileLayer({
-          source: new XYZ({
-            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-          })
-        })
-      ],
-      view: new View({
-        center: fromLonLat(center),
-        zoom: zoom
-      })
-    })
+    // TODO: this feels so wrong
+    InitMap()
   })
 
-  return <div id="map" style={{'width': '100%', 'height': '800px'}} />
+  return <div id="map" style={{'width': '100%', 'height': '400px'}} />
 }
 
 export default OpenMap
