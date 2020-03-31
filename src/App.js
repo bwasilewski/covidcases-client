f505ca381c9044a03c1e260c2c14c9f6a69b19f5
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-03-31 15:58:21 -0500

    remove cruft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react'
 import dotenv from 'dotenv'
 import './App.css'
-import Page from './components/Page'
 import About from './containers/About'
 import Index from './containers/Index'
-import { Title } from 'bloomer'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
-
 
 dotenv.config()
 const history = createBrowserHistory()
 
 function App() {
   return (
-    <div>
-      <Router history={history}>
-        <Route exact path="/" component={Index} />
-        <Route exact path="/about" component={About} />
-      </Router>
-    </div>
+    <Router history={history}>
+      <Route exact path="/" component={Index} />
+      <Route exact path="/about" component={About} />
+    </Router>
   )
 }
 
