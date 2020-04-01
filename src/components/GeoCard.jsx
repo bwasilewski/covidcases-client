59504653dfda99a154df3711f22aae06e1511824
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-03-31 23:47:30 -0500

    add content to geocard

--- src/components/GeoCard.jsx
+++ src/components/GeoCard.jsx
@@ -5,10 +5,31 @@ import {
   Title } from 'bloomer'
 
 const GeoCard = props => {
+  const { address } = props.data
   console.log('Geo Data: ', props.data)
+
   return (
     <Card title="Geography Data">
-      <p>{ props.data.display_name }</p>
+      <Table>
+        <tbody>
+          <tr>
+            <td><small>County</small></td>
+            <td>{ address.county }</td>            
+          </tr>
+          <tr>
+            <td><small>State</small></td>
+            <td>{ address.state }</td>
+          </tr>
+          <tr>
+            <td><small>Postal Code</small></td>
+            <td>{ address.postcode }</td>
+          </tr>
+          <tr>
+            <td><small>Country</small></td>
+            <td>{ address.country }</td>
+          </tr>
+        </tbody>
+      </Table>
     </Card>
   )
 }
