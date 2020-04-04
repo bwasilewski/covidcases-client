1cbb09b6a034e392b93bf0d642fc4ae9e5cb3f22
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-04-03 22:18:41 -0500

    add some boilerplate to the about page

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Page from '../components/Page'
 import PageHeader from '../components/PageHeader'
+import { 
+  Section,
+  Title 
+} from 'bloomer'
 
 const About = props => {
   return (
     <Page>
-      <PageHeader title="About" />
+      <p>CovidCases.io was built by <a href="http://benwas.com">Benjamin Wasilewski</a> during the great Coronavirus Lockdown of the year of our L0RD 2020</p>
+      <p>It was built with the help of the following resources:</p>
+
+      <ul>
+        <li>Maps with <a href="https://openlayers.org/">OpenLayers</a></li>
+        <li>Data with the <a href="https://covidnow.docs.apiary.io/">CovidNow API</a></li>
+      </ul>
+
     </Page>
   )
 }
