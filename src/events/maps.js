d12223c8edd828afd833b03fa45fa3fb9564167e
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-04-05 00:53:40 -0500

    restore our little masked man

--- src/events/maps.js
+++ src/events/maps.js
@@ -1,9 +1,10 @@
 import { fromLonLat } from 'ol/proj'
 import Map from 'ol/Map'
 import View from 'ol/View'
+import GeoJSON from 'ol/format/GeoJSON'
 import TileLayer from 'ol/layer/Tile'
 import VectorLayer from 'ol/layer/Vector'
-import XYZ from 'ol/source/XYZ'
+import OSM from 'ol/source/OSM'
 import VectorSource from 'ol/source/Vector'
 import Feature from 'ol/Feature'
 import Point from 'ol/geom/Point'
@@ -17,9 +18,7 @@ export const InitMap = (center, zoom) => {
 
   const iconFeature = new Feature({
     geometry: new Point(fromLonLat(center)),
-    name: 'Null Island',
-    fart: true,
-    gay: false
+    name: 'Null Island'
   })
 
   const iconStyle = new Style({
@@ -40,20 +39,36 @@ export const InitMap = (center, zoom) => {
   })
 
   iconFeature.setStyle(iconStyle)
+
+  // const bgLayer = new TileLayer({
+  //   source: new XYZ({
+  //     url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+  //   })
+  // })
+
+  const rasterLayer = new TileLayer({
+    source: new OSM()
+  });
+
+ const iconLayer = new VectorLayer({
+   source: new VectorSource({
+     features: [iconFeature]
+   })
+ })
+
+//  var statesLayer = new VectorLayer({
+//    source: new VectorSource({
+//      url: 'public/geojson/us-states.json',
+//      format: new GeoJSON()
+//    })
+//  });
   
   const map = new Map({
     target: 'map',
     layers: [
-      new TileLayer({
-        source: new XYZ({
-          url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        })
-      }),
-      new VectorLayer({
-        source: new VectorSource({
-          features: [iconFeature]
-        })
-      }),
+      rasterLayer,
+      // statesLayer,
+      iconLayer,
     ],
     view: new View({
       center: fromLonLat(center),
@@ -63,10 +78,9 @@ export const InitMap = (center, zoom) => {
 
   map.addOverlay(popup)
 
-  document.getElementById('map').addEventListener('click', (ev) => {
-    console.log('Click: ', ev)
+  map.on('click', function (e) {
+    console.log('Event: ', e)
   })
-
 
   // display popup on click
   // map.on('click', function(evt) {
