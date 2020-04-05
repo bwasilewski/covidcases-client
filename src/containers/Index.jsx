c513beca81aaf035fcda37054046aebc419a407b
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-04-04 21:49:52 -0500

    add some UI

--- src/containers/Index.jsx
+++ src/containers/Index.jsx
@@ -5,7 +5,7 @@ import CovidCard from '../components/CovidCard'
 import ZipcodeCard from '../components/ZipcodeCard'
 import { geoLocateUser, getCovidByAddress } from '../events'
 import { InitMap } from '../events/maps'
-import { Columns, Column } from 'bloomer'
+import { Button, Columns, Column } from 'bloomer'
 import { Helmet } from 'react-helmet'
 
 import { GeoContext } from '../contexts/geography'
@@ -14,7 +14,7 @@ const Index = props => {
   const [geo, setGeo] = useContext(GeoContext)
   const [covid, setCovid] = useState(null)
   const [disallowLocation, setDisallowLocation] = useState(null)
-
+  const [granularity, setGranularity] = useState('county')
   const locationOptions = {
     enableHighAccuracy: true,
     timeout: 5000,
@@ -31,7 +31,7 @@ const Index = props => {
     InitMap([position.coords.longitude, position.coords.latitude], 10)
   }
 
-  const locationFailure = error => setDisallowLocation(true)
+  const locationFailure = error => setGeo(false)
 
   const getLocation = () => navigator.geolocation.getCurrentPosition(locationSuccess, locationFailure, locationOptions)
 
@@ -44,7 +44,7 @@ const Index = props => {
       <Helmet>
         <title>covidcases.io</title>
       </Helmet>
-      { geo === null && (
+      { geo === false && (
         <Columns>
           <Column></Column>
           <Column isSize="1/3">
@@ -54,10 +54,17 @@ const Index = props => {
         </Columns>
       )}
       { geo !== null && (
-
         <Columns>
           <Column>
-            { geo !== null && <GeoCard title="Geography Data" data={geo} /> }
+            <p>{ geo.address.county }, { geo.address.state }, { geo.address.country }</p>
+            <div>
+              <p>See stats by:</p>
+              <ul>
+                <li><Button>County</Button></li>
+                <li><Button>State</Button></li>
+                <li><Button>Country</Button></li>
+              </ul>
+            </div>
             { covid !== null && <CovidCard title="Covid Data" data={covid} /> }
           </Column>
           <Column isSize="2/3">
