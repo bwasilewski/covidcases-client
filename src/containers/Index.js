337608ae5d487d25d55743418163afe395f50973
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-07-19 22:59:45 -0500

    prettier

--- src/containers/Index.js
+++ src/containers/Index.js
@@ -11,125 +11,125 @@ import { Helmet } from 'react-helmet'
 import { GeoContext, LoadingContext } from '../contexts'
 
 const Index = props => {
-  const [geo, setGeo] = useContext(GeoContext)
-  const [covid, setCovid] = useState(null)
-  const [loading, setLoading] = useContext(LoadingContext)
-  const [error, setError] = useState(null)
-  const [noGeo, setNoGeo] = useState(false)
-  const [confirmed, setConfirmed] = useState(0)
-  const [recovered, setRecovered] = useState(0)
-  const [deaths, setDeaths] = useState(0)
- 
-  const handleGeo = response => {
-    locationSuccess({
-      coords: {
-        latitude: parseFloat(response.lat),
-        longitude: parseFloat(response.lon)
-      }
-    })
-  }
+	const [geo, setGeo] = useContext(GeoContext)
+	const [covid, setCovid] = useState(null)
+	const [loading, setLoading] = useContext(LoadingContext)
+	const [error, setError] = useState(null)
+	const [noGeo, setNoGeo] = useState(false)
+	const [confirmed, setConfirmed] = useState(0)
+	const [recovered, setRecovered] = useState(0)
+	const [deaths, setDeaths] = useState(0)
 
-  const zipcodeSubmit = ev => {
-    const { target } = ev
-    const { elements } = target
+	const handleGeo = response => {
+		console.log(response)
+		locationSuccess({
+			coords: {
+				latitude: parseFloat(response.lat),
+				longitude: parseFloat(response.lon),
+			},
+		})
+	}
 
-    ev.preventDefault()
+	const zipcodeSubmit = ev => {
+		const { target } = ev
+		const { elements } = target
+
+		ev.preventDefault()
 		setLoading(true)
 
-    geoLocateByZip(elements.namedItem('zipcode').value)
-      .then(response => handleGeo(response))
-      .catch(error => console.error(error))
-  }
+		geoLocateByZip(elements.namedItem('zipcode').value)
+			.then(response => handleGeo(response))
+			.catch(error => console.error(error))
+	}
 
-  const locationSuccess = async position => {
-    setLoading(true)
-    setNoGeo(false)
-   
+	const locationSuccess = async position => {
+		setLoading(true)
+		setNoGeo(false)
+
 		const geoResponse = await geoLocateUser(position)
-    const covidResponse = await getCovidByAddress(geoResponse.display_name)
-    
+		const covidResponse = await getCovidByAddress(geoResponse.display_name)
+
 		setGeo(geoResponse)
-    setCovid(covidResponse)
+		setCovid(covidResponse)
 		const { county } = covidResponse.cases
-    setDeaths(county.deaths)
-    setRecovered(county.recovered)
-    setConfirmed(county.confirmed)
-    setLoading(false)
+		setDeaths(county.deaths)
+		setRecovered(county.recovered)
+		setConfirmed(county.confirmed)
+		setLoading(false)
 
-    InitMap([position.coords.longitude, position.coords.latitude], 10)
-  }
+		InitMap([position.coords.longitude, position.coords.latitude], 10)
+	}
 
-  const locationFailure = error => { 
-    setGeo(null)
-    setNoGeo(true)
-    setLoading(false)
-		// TODO: need to display the error message in some way 
+	const locationFailure = error => {
+		setGeo(null)
+		setNoGeo(true)
+		setLoading(false)
+		// TODO: need to display the error message in some way
 		setError(error)
-  }
+	}
 
-  useEffect(() => {
-    if ( navigator.geolocation ) {
-      // browser is capable of geolocation
-      // let's ask permission for that data
-      navigator.geolocation.getCurrentPosition(
-        locationSuccess, 
-        locationFailure, 
-        {
-          enableHighAccuracy: true,
-          timeout: 5000,
-          maximumAge: 0
-        }
+	useEffect(() => {
+		if (navigator.geolocation) {
+			// browser is capable of geolocation
+			// let's ask permission for that data
+			navigator.geolocation.getCurrentPosition(
+				locationSuccess,
+				locationFailure,
+				{
+					enableHighAccuracy: true,
+					timeout: 5000,
+					maximumAge: 0,
+				}
+			)
+		} else {
+			// browser does not support geolocation
+			setNoGeo(true)
+		}
+	}, [])
 
-      )
-    } else {
-      // browser does not support geolocation
-      setNoGeo(true)
-    }
-  }, [])
-
-  return (
-    <Page>
-      <Helmet>
-        <title>covidcases.io</title>
-      </Helmet>
-      { noGeo === true && loading === false && (
-        <Columns>
-          <Column></Column>
-          <Column isSize="1/3">
-            <ZipcodeCard submit={zipcodeSubmit} />
-          </Column>
-          <Column></Column>
-        </Columns>
-      )}
-      { loading === true && <Loading /> }
-      { geo && (
-        <>
-          <Title isSize="5">
-						county: { geo.address.county }, state: { geo.address.state }
-          </Title>
-          {/* <StateToggle onChange={handleSwitch} checked={viewByState} /> */}
-          <Columns>
-            <Column isSize="1/2">
-              <div id="map" style={{'width': '100%', 'height': '600px'}}><div id="popup"></div></div>
-            </Column>
-            <Column hasTextAlign="centered">
-              <Card>
-                Cases: { confirmed }
-              </Card>
-            </Column>
-            <Column hasTextAlign="centered">
-              <Card>Deaths: { deaths }</Card>
-            </Column>
-            { recovered > 0 && (
-              <Column hasTextAlign="centered">
-                <Card>Recovered: { recovered }</Card>
-              </Column>
-            )}
-          </Columns>
-        </>
-      )}
-    </Page>
-  )
+	return (
+		<Page>
+			<Helmet>
+				<title>covidcases.io</title>
+			</Helmet>
+			{noGeo === true && loading === false && (
+				<Columns>
+					<Column></Column>
+					<Column isSize="1/3">
+						<ZipcodeCard submit={zipcodeSubmit} />
+					</Column>
+					<Column></Column>
+				</Columns>
+			)}
+			{loading === true && <Loading />}
+			{geo && (
+				<>
+					<Title isSize="5">
+						county: {geo.address.county}, state: {geo.address.state}
+					</Title>
+					{/* <StateToggle onChange={handleSwitch} checked={viewByState} /> */}
+					<Columns>
+						<Column isSize="1/2">
+							<div id="map" style={{ width: '100%', height: '600px' }}>
+								<div id="popup"></div>
+							</div>
+						</Column>
+						<Column hasTextAlign="centered">
+							<Card>Cases: {confirmed}</Card>
+						</Column>
+						<Column hasTextAlign="centered">
+							<Card>Deaths: {deaths}</Card>
+						</Column>
+						{recovered > 0 && (
+							<Column hasTextAlign="centered">
+								<Card>Recovered: {recovered}</Card>
+							</Column>
+						)}
+					</Columns>
+				</>
+			)}
+		</Page>
+	)
 }
 
 export default Index
--- src/events/index.js
+++ src/events/index.js
@@ -1,42 +1,49 @@
 import axios from 'axios'
 
-const BASEURL = process.env.REACT_APP_BACKEND 
+const BASEURL = process.env.REACT_APP_BACKEND
 
 export const geoLocateUser = position => {
-  return new Promise((resolve, reject) => {
-    axios.get(`${BASEURL}/geolocate?lat=${position.coords.latitude}&lng=${position.coords.longitude}`)
-      .then(response => resolve(response.data))
-      .catch(error => reject(error)) 
-  })
+	return new Promise((resolve, reject) => {
+		axios
+			.get(
+				`${BASEURL}/geolocate?lat=${position.coords.latitude}&lng=${position.coords.longitude}`
+			)
+			.then(response => resolve(response.data))
+			.catch(error => reject(error))
+	})
 }
 
 export const geoLocateByZip = zip => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const locationData = await axios.get(`${BASEURL}/locatebyzip?zip=${zip}`)
-      const geoData = await geoLocateUser({
-        coords: { 
-          latitude: locationData.data.lat,
-          longitude: locationData.data.lng
-        }
-      })
-      resolve(geoData)
-    } catch (error) { reject(error) }
-  })
+	return new Promise(async (resolve, reject) => {
+		try {
+			const locationData = await axios.get(`${BASEURL}/locatebyzip?zip=${zip}`)
+			const geoData = await geoLocateUser({
+				coords: {
+					latitude: locationData.data.lat,
+					longitude: locationData.data.lon,
+				},
+			})
+			resolve(geoData)
+		} catch (error) {
+			reject(error)
+		}
+	})
 }
 
 export const getCovidByAddress = address => {
-  return new Promise((resolve, reject) => {
-    axios.get(`https://api.covidnow.com/v1/local/finder?address=${address}`)
-      .then(response => resolve(response.data))
-      .catch(error => reject(error))
-  })
+	return new Promise((resolve, reject) => {
+		axios
+			.get(`https://api.covidnow.com/v1/local/finder?address=${address}`)
+			.then(response => resolve(response.data))
+			.catch(error => reject(error))
+	})
 }
 
 export const getGlobalCovidStats = () => {
-  return new Promise((resolve, reject) => {
-    axios.get(`https://api.covidnow.com/v1/global/stats`)
-      .then(response => resolve(response.data))
-      .catch(error => reject(error))
-  })
+	return new Promise((resolve, reject) => {
+		axios
+			.get(`https://api.covidnow.com/v1/global/stats`)
+			.then(response => resolve(response.data))
+			.catch(error => reject(error))
+	})
 }
