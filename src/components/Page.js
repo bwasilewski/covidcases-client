50357a57428aecde6dca9fbf05c1b7dc596c9958
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-07-31 11:31:16 -0500

    move header to page component

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import Header from './Header'
 import { Container, Section } from 'bloomer'
 
 const Page = props => {
   return (
-    <Section>
-      <Container>
-        { props.children }
-      </Container>
-    </Section>
+		<>
+			<Header geo={props.geo} />
+			<Section>
+				<Container>
+					{ props.children }
+				</Container>
+			</Section>
+		</>
   )
 }
 
